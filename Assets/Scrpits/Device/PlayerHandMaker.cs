033565d4d59824ed58502c324c1ad00637237467
196kakinuma <196thinline@gmail.com>
2017-09-16 14:07:21 +0900

    Initial commit

---
+++ Assets/Scrpits/Device/PlayerHandMaker.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+namespace Device
+{
+    public class PlayerHandMaker : NetworkBehaviour
+    {
+        [SerializeField]
+        GameObject rightHand;
+        [SerializeField]
+        GameObject leftHand;
+
+        // Use this for initialization
+        [ClientCallback]
+        void Start ()
+        {
+            if ( Networks.NetworkInitializer.Instance.cameraType == CameraType.VR )
+            {
+                CmdCreateHands (this.gameObject);
+            }
+
+
+        }
+
+        [Command]
+        private void CmdCreateHands ( GameObject obj )
+        {
+            Debug.Log ("create hand");
+            NetworkServer.SpawnWithClientAuthority (Instantiate (rightHand), obj);
+            NetworkServer.SpawnWithClientAuthority (Instantiate (leftHand), obj);
+        }
+        // Update is called once per frame
+        void Update ()
+        {
+
+        }
+    }
+}
---
+++ Assets/Scrpits/Main/MainSceneinitializer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Networks;
+using UnityEngine.Networking;
+namespace Main
+{
+    public class MainSceneinitializer : MonoBehaviour
+    {
+
+        [SerializeField]
+        GameObject trackerMasterPref;
+
+        // Use this for initialization
+        void Start ()
+        {
+            if ( NetworkInitializer.Instance.cameraType == CameraType.VR )
+            {
+                //MR用のトラッカーマスターを生成
+                NetworkServer.Spawn (Instantiate (trackerMasterPref));
+            }
+        }
+
+        // Update is called once per frame
+        void Update ()
+        {
+
+        }
+    }
+}

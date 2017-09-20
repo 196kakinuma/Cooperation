e56370df2f2f2cd601b77919c7aae3379bd366e9
196kakinuma <196thinline@gmail.com>
2017-09-20 14:44:16 +0900

    WordPushGame板の生成
    星などのアセットをインポート（未使用）

---
+++ Assets/Scrpits/Games/WordPushGame/WPGWordButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Games.WordpushGame
+{
+    public class WPGWordButton : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start ()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update ()
+        {
+
+        }
+    }
+}

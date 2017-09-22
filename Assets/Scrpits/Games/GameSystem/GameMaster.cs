8a72dc72f015f7b71979f7cda6019f1a3272d181
196kakinuma <196thinline@gmail.com>
2017-09-22 13:12:06 +0900

    Masterにゲーム開始のロジック追加(未完成)

--- Assets/Scrpits/Games/GameSystem/GameMaster.cs
+++ Assets/Scrpits/Games/GameSystem/GameMaster.cs
@@ -11,23 +11,61 @@ namespace Games.GameSystem
     /// </summary>
     public class GameMaster : SingletonMonoBehaviour<GameMaster>
     {
+        //システム系
         [SerializeField]
         GameObject startButtonPref;
-
         StartButtonHandler startButton;
+
+        //ゲームプレハブ系
+        [SerializeField]
+        GameObject WPGPref;
+        Games.WordPushGame.WPGMaster wpgMaster;
 
         // Use this for initialization
         void Start ()
         {
+            //startボタン
             var b = Instantiate (startButtonPref);
             this.startButton = b.GetComponent<StartButtonHandler> ();
             NetworkServer.Spawn (b);
+
+            //wpg
+            var wpg = Instantiate (WPGPref);
+            this.wpgMaster = wpg.GetComponent<Games.WordPushGame.WPGMaster> ();
+            NetworkServer.Spawn (wpg);
         }
 
         // Update is called once per frame
         void Update ()
         {
 
+        }
+
+        /// <summary>
+        /// startButtonから呼ばれる.ゲームを開始する
+        /// </summary>
+        public IEnumerator StartGame ( Coroutine buttonAnimation )
+        {
+            Coroutine prepare = StartCoroutine (StartPrepare ());
+            yield return prepare;
+            yield return buttonAnimation;
+
+            //タイマーなどを開始
+            //敵を動かし始める
+            //記録を取り始める
+
+            //スタートの表示
+
+
+        }
+
+        /// <summary>
+        /// ゲームの設定を行う
+        /// </summary>
+        /// <returns></returns>
+        IEnumerator StartPrepare ()
+        {
+            yield return null;
         }
     }
 }

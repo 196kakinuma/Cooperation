fa41768ec53c18e71dc10b99422c679c469db002
196kakinuma <196thinline@gmail.com>
2017-10-13 18:11:58 +0900

    データ書き出しクラスの実装

---
+++ Assets/Scrpits/Games/GameSystem/ExcelWriter.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using IkLibrary.Unity;
+using UnityEngine;
+using System.IO;
+namespace Games.GameSystem
+{
+    public class ExcelWriter : SingletonMonoBehaviour<ExcelWriter>
+    {
+        public string day;
+        public string UserName;
+        public string memo = "";
+
+        string extension = ".scv";
+
+        string filename;
+        StreamWriter sw;
+        // Use this for initialization
+        void Start ()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update ()
+        {
+
+        }
+
+        public void InitializeFile ()
+        {
+            filename = name + extension;
+
+            FileStream fs = File.Create (Application.dataPath + "/ExperimentData/" + filename);
+
+            Debug.Log (fs.Name);
+            // ファイルストリームを閉じて、変更を確定させる
+            // 呼ばなくても using を抜けた時点で Dispose メソッドが呼び出される
+            fs.Close ();
+
+
+            FileInfo fi = new FileInfo (Application.dataPath + "/ExperimentData/" + filename);
+            sw = fi.AppendText ();
+            sw.WriteLine ("test output");
+        }
+
+        public void InitWriteUserInfo ()
+        {
+            sw.WriteLine (day + ",");
+            sw.WriteLine (name + ",");
+
+        }
+
+        public void WriteWord ( string word )
+        {
+            sw.Write (word + ",");
+        }
+
+        public void CloseFile ()
+        {
+            sw.Flush ();
+            sw.Close ();
+        }
+    }
+}

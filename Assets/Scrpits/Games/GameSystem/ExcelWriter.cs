66a3f3414377df835f61004ce9a0e5053f899da7
196kakinuma <196thinline@gmail.com>
2017-10-24 18:59:45 +0900

    UWPのエラー回避

--- Assets/Scrpits/Games/GameSystem/ExcelWriter.cs
+++ Assets/Scrpits/Games/GameSystem/ExcelWriter.cs
@@ -16,7 +16,7 @@ namespace Games.GameSystem
         public void InitializeFile ( string name )
         {
             filename = name + extension;
-
+#if !UNITY_WSA_10_0
 
             FileStream fs = File.Create (Application.dataPath + "/ExperimentData/" + filename);
 
@@ -24,7 +24,7 @@ namespace Games.GameSystem
             // ファイルストリームを閉じて、変更を確定させる
             // 呼ばなくても using を抜けた時点で Dispose メソッドが呼び出される
             fs.Close ();
-
+#endif
 
             FileInfo fi = new FileInfo (Application.dataPath + "/ExperimentData/" + filename);
             sw = fi.AppendText ();
@@ -54,7 +54,9 @@ namespace Games.GameSystem
         public void CloseFile ()
         {
             sw.Flush ();
+#if !UNITY_WSA_10_0
             sw.Close ();
+#endif
         }
     }
 }

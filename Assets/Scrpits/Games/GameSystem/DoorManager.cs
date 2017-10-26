934a1938a7c457d89495b2defb89a0d61d8f0582
196kakinuma <196thinline@gmail.com>
2017-10-26 11:59:07 +0900

    デバッグ時以外は3扉使うよう変更

--- Assets/Scrpits/Games/GameSystem/DoorManager.cs
+++ Assets/Scrpits/Games/GameSystem/DoorManager.cs
@@ -102,8 +102,10 @@ namespace Games.GameSystem
             else //まだenemyはどこの部屋にも入っていなかった
             {
                 //入る部屋を決める
-                //int decideRoom = Random.Range (0, doors.Length);
-                int decideRoom = 1;
+
+                int decideRoom = Random.Range (0, doors.Length);
+                if ( GameSettings.Instance.debug )
+                    decideRoom = 1;
                 if ( enemyRoomList.ContainsValue (doors[decideRoom]) || blankRoomList.Contains (doors[decideRoom]) )//入ろうとした部屋にすでにだれか入っていた
                 {
                     Debug.Log ("だれかすでにいた");

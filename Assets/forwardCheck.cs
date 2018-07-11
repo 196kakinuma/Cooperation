f2714b5d3c0b01b6e36bcbd65509aaa1ec1b2c30
196kakinuma <196thinline@gmail.com>
2018-07-11 19:05:40 +0900

    forward確認

---
+++ Assets/forwardCheck.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace C2.Test
+{
+    public class forwardCheck : MonoBehaviour
+    {
+        [SerializeField]
+        GameObject Cube;
+        // Use this for initialization
+        void Start ()
+        {
+            Instantiate (Cube, this.transform.forward, Quaternion.identity, this.transform);
+        }
+
+        // Update is called once per frame
+        void Update ()
+        {
+
+        }
+    }
+}

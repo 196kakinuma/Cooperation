34589a33ef3b24145082dd42ef9660d142ca1846
196kakinuma <196thinline@gmail.com>
2018-07-12 14:19:06 +0900

    VR環境にMRPlayer表示

---
+++ Assets/Scrpits/2/Player/MRPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace C2.Palyer
+{
+    public class MRPlayer : MonoBehaviour
+    {
+        [SerializeField]
+        GameObject tracker;
+        // Use this for initialization
+        void Start ()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update ()
+        {
+            gameObject.transform.position = tracker.transform.position;
+        }
+    }
+}

a0d7765bcd1ec88857bf3f16b5a6f411a4776232
196kakinuma <196thinline@gmail.com>
2018-07-20 14:03:48 +0900

    Metaの回転を無効か

---
+++ Assets/HMDTrackCanceler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HMDTrackCanceler : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start ()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        positionParent.transform.position = ( -1 * UnityEngine.XR.InputTracking.GetLocalPosition (UnityEngine.XR.XRNode.CenterEye) );
+
+    }
+}

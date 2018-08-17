c83baf6ab1a0fbe02fd41c266615aa14dfe7eac7
196kakinuma <196thinline@gmail.com>
2018-08-17 13:57:14 +0900

    rotateとscaleテスト追加
    コントローラで計測をするように

--- Assets/Scrpits/2/Test/MetaOffsetRecorder.cs
+++ Assets/Scrpits/2/Test/MetaOffsetRecorder.cs
@@ -6,56 +6,81 @@ using IkLibrary.Unity;
 
 namespace C2.Test
 {
+    enum MeasureTest
+    {
+        ROTATE,
+        MOVE
+    }
     public class MetaOffsetRecorder : MonoBehaviour
     {
         [SerializeField]
         GameObject[] offsetObj;
+        SteamVR_Controller.Device device;
+        [SerializeField]
+        MeasureTest testMode = MeasureTest.ROTATE;
         [SerializeField]
         GameObject tracker;
         ExcelWriter exw;
         [SerializeField]
         string exptag = "-1";
 
+        int total = 0;
+        [SerializeField]
         Vector3[] keeper;
         int count = 0;
         // Use this for initialization
         void Start ()
         {
-
-            keeper = new Vector3[12];
+            var trackedobj = GetComponent<SteamVR_TrackedObject> ();
+            device = SteamVR_Controller.Input (( int ) trackedobj.index);
+            switch ( testMode )
+            {
+                case MeasureTest.ROTATE:
+                    total = 12;
+                    break;
+                case MeasureTest.MOVE:
+                    total = 8;
+                    break;
+                default:
+                    total = 12;
+                    break;
+            }
+            keeper = new Vector3[total];
         }
 
 
         // Update is called once per frame
         void Update ()
         {
-            if ( Input.GetKeyUp (KeyCode.Space) )
+            if ( device.GetPressUp (SteamVR_Controller.ButtonMask.Trigger) )
             {
-                Vector3 target;
-                //初期誤差を記憶
-                if ( count < 4 )
                 {
-                    target = offsetObj[count].transform.position;
-                }
-                else
-                {
-                    target = keeper[count % 4];
-                }
-                keeper[count] = tracker.transform.position - target;
-                count++;
-
-                if ( count == 12 )//終了
-                {
-                    exw = new ExcelWriter ();
-                    exw.InitializeFile ("MetaOffset" + exptag);
-                    foreach ( var a in keeper )
+                    Vector3 target;
+                    //初期誤差を記憶
+                    if ( count < 4 )
                     {
-                        exw.WriteWords (a.x.ToString ());
-                        exw.WriteWords (a.y.ToString ());
-                        exw.WriteWords (a.z.ToString ());
-                        exw.WriteNewLine ();
+                        target = offsetObj[count].transform.position;
                     }
-                    exw.CloseFile ();
+                    else
+                    {
+                        target = keeper[count % 4];
+                    }
+                    keeper[count] = tracker.transform.position - target;
+                    count++;
+
+                    if ( count == total )//終了
+                    {
+                        exw = new ExcelWriter ();
+                        exw.InitializeFile ("MetaError" + testMode.ToString () + exptag);
+                        foreach ( var a in keeper )
+                        {
+                            exw.WriteWords (a.x.ToString ());
+                            exw.WriteWords (a.y.ToString ());
+                            exw.WriteWords (a.z.ToString ());
+                            exw.WriteNewLine ();
+                        }
+                        exw.CloseFile ();
+                    }
                 }
             }
         }

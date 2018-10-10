b2d37fd6b1004f8e26f43fae5917f82c6e493ac8
196kakinuma <196thinline@gmail.com>
2018-10-10 16:50:59 +0900

    5秒間隔でテスト

--- Assets/Scrpits/2/Block/BlockSpawner.cs
+++ Assets/Scrpits/2/Block/BlockSpawner.cs
@@ -11,18 +11,29 @@ namespace C2.Block
         [SerializeField]
         GameObject block;
 
+        float time=0f;
         // Use this for initialization
         void Start()
         {
-            GameObject a = Instantiate(block);
-            a.transform.position = new Vector3(0,1,-5f);
-            NetworkServer.Spawn(a);
+
         }
 
         // Update is called once per frame
         void Update()
         {
+            time += Time.deltaTime;
+            if(time>5f)
+            {
+                Create();
+                time = 0f;
+            }
+        }
 
+        void Create()
+        {
+            GameObject a = Instantiate(block);
+            a.transform.position = new Vector3(0, 1, -5f);
+            NetworkServer.Spawn(a);
         }
     }
 }

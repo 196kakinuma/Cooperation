450eafb503a43ad32db6494ef23ee4cec2bf24f7
196kakinuma <196thinline@gmail.com>
2018-10-10 16:32:52 +0900

    VRZoneで出現するように

---
+++ Assets/Scrpits/2/Block/BlockSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace C2.Block
+{
+
+    public class BlockSpawner : MonoBehaviour
+    {
+        [SerializeField]
+        GameObject block;
+
+        // Use this for initialization
+        void Start()
+        {
+            GameObject a = Instantiate(block);
+            a.transform.position = new Vector3(0,1,-4f);
+            NetworkServer.Spawn(a);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
---
+++ Assets/Scrpits/2/Test/ObjVanish.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace C2.Test
+{
+
+    public class ObjVanish : MonoBehaviour
+    {
+
+        [SerializeField]
+        GameObject block;
+
+        // Use this for initialization
+        void Start()
+        {
+            if (Networks.NetworkInitializer.Instance.cameraType == CameraType.VR)
+            {
+                block.SetActive(false);
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            Debug.Log("aa");
+            if(other.gameObject.tag=="VRZone")
+            {
+                block.gameObject.SetActive(true);
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if(other.gameObject.tag=="VRZone")
+            {
+                block.gameObject.SetActive(false);
+            }
+        }
+    }
+}
